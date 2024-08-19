6e88863bf55ff6dec335dfa1e508f22f9e6e0b8d
Bueno <gabrielhbs@outlook.com.br>
2024-08-19 20:21:39 +0100

    login com discord, callback e troca do token por sessao de auth funcionando

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -1,22 +1,26 @@
-
-
 export const getURL = (path: string = '') => {
     // Check if NEXT_PUBLIC_SITE_URL is set and non-empty. Set this to your site URL in production env.
     let url =
         process?.env?.NEXT_PUBLIC_SITE_URL &&
-            process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
+        process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
             ? process.env.NEXT_PUBLIC_SITE_URL
             : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
             process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-                process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
+            process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
                 ? process.env.NEXT_PUBLIC_VERCEL_URL
                 : // If neither is set, default to localhost for local development.
-                'localhost:3000/';
+                'localhost:3000';
 
     // Trim the URL and remove trailing slash if exists.
     url = url.replace(/\/+$/, '');
-    // Make sure to include `https://` when not localhost.
-    url = url.includes('http') ? url : `https://${url}`;
+
+    // Make sure to include `https://` only when not localhost.
+    if (!url.includes('http') && !url.includes('localhost')) {
+        url = `https://${url}`;
+    } else if (url.includes('localhost')) {
+        url = `http://${url}`;
+    }
+
     // Ensure path starts without a slash to avoid double slashes in the final URL.
     path = path.replace(/^\/+/, '');
 
